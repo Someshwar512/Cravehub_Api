46b09fa164553b779f24e24517dce36e01845058
Somwshwar <sonusolat512@gmail.com>
2024-03-20 22:38:22 +0530

    entites changes

--- src/database/entities/City.ts
+++ src/database/entities/City.ts
@@ -14,34 +14,20 @@ export class City {
   @Column({ type: 'enum', enum: Status, default: Status.ACTIVE })
   status: Status;
 
-  // @ManyToOne(() => State, state => state.id)
-  // state: State;
-
-  // @ManyToOne(() => Zipcode, zipcode => zipcode.id)
-  // zipcode: Zipcode;
+  
 
  
 
   @OneToMany(() => Zipcode, zipcode => zipcode.city)
   zipcodes: Zipcode[];
 
-  // @ManyToOne(() => State, state => state.cities)
+ 
 
 
   @ManyToOne(() => State, state => state.cities)
-  @JoinColumn({ name: 'state_id' }) // Adjust to the correct column name representing the state relationship
+  @JoinColumn({ name: 'state_id' }) 
   state: State;
 
-
-  // @JoinColumn({ name: 'state_id' })
-  // state: State;
-  // @OneToOne(() => State)
-  // @JoinColumn()
-  // state: State;
-
-  // @OneToOne(() => Zipcode)
-  // @JoinColumn()
-  // zipcode: Zipcode;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   created_on: Date;
